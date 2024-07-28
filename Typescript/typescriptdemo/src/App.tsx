bb02e94fb16c7ca4b9b8d74030f5c429c2985055
sabb12 <sabb0923@gmail.com>
2024-07-28 21:46:08 +0900

    installed react with typescript and added features

---
+++ Typescript/typescriptdemo/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Enter from "./component/Enter";
+import Pagination1 from "./component/Pagination1";
+import ResponsiveWebsite from "./component/ResponsiveWebsite";
+
+function App() {
+  return (
+    <div className="App">
+      <Enter />
+      <Pagination1 />
+      <ResponsiveWebsite />
+    </div>
+  );
+}
+
+export default App;
